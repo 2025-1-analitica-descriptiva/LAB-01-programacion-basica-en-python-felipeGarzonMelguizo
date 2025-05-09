75b984d1e09f831f6c7526ca6e08bb46cfa1c8ee
felipeGarzonMelguizo <69770704+Flying-Spaghetti-Monster69@users.noreply.github.com>
2025-05-09 12:18:13 -0500

    respuesta pregunta 2

--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -5,6 +5,7 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+import csv
 
 def pregunta_02():
     """
@@ -15,3 +16,21 @@ def pregunta_02():
     [('A', 8), ('B', 7), ('C', 5), ('D', 6), ('E', 14)]
 
     """
+
+    letter_counts = {}
+
+
+    with open('files/input/data.csv', 'r') as file:
+        csv_reader = csv.reader(file, delimiter='\t') 
+        for row in csv_reader:
+            letter = row[0]
+            letter_counts[letter] = letter_counts.get(letter, 0) + 1
+    
+    # Convert dictionary to list of tuples and sort
+    result = [(letter, count) for letter, count in letter_counts.items()]
+    result.sort()
+        
+
+    return result
+
+pregunta_02()
