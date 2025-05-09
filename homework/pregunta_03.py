2b60d94940e66195ef27451294150c5da840210c
felipeGarzonMelguizo <69770704+Flying-Spaghetti-Monster69@users.noreply.github.com>
2025-05-09 12:47:50 -0500

    respuesta pregunta 3

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -5,6 +5,8 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+from homework.getData import getCsvData
+
 
 def pregunta_03():
     """
@@ -15,3 +17,18 @@ def pregunta_03():
     [('A', 53), ('B', 36), ('C', 27), ('D', 31), ('E', 67)]
 
     """
+    letter_counts = {}
+
+    data = getCsvData()
+
+    for row in data:
+        letter = row[0]
+        number = row[1]
+        letter_counts[letter] = letter_counts.get(letter, 0) + int(number)
+    
+    result = [(letter, count) for letter, count in letter_counts.items()]
+    result.sort()
+
+    return result
+
+
