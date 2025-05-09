eaae5c2afcbaeabf8b8df0e8edc78d66f9510286
felipeGarzonMelguizo <69770704+Flying-Spaghetti-Monster69@users.noreply.github.com>
2025-05-09 13:19:29 -0500

    respuesta pregunta 5

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -5,6 +5,7 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+from homework.getData import getCsvData
 
 def pregunta_05():
     """
@@ -15,3 +16,24 @@ def pregunta_05():
     [('A', 9, 2), ('B', 9, 1), ('C', 9, 0), ('D', 8, 3), ('E', 9, 1)]
 
     """
+
+    letters = {}
+
+    data = getCsvData()
+
+    for row in data:
+        letter = row[0]
+        number = int(row[1])
+        letterHasValue = letters.get(letter, False)
+        if letterHasValue == False:
+            letters[letter] = [number, None]
+        else:
+            maximun = max(number,letters[letter][0])
+            minimun = min(number,letters[letter][1]) if letters[letter][1] != None else min(number,letters[letter][0])
+            letters[letter] = [maximun,minimun]
+
+    
+    result = [(letter, maxAndMin[0],maxAndMin[1]) for letter, maxAndMin in letters.items()]
+    result.sort()
+
+    return result
