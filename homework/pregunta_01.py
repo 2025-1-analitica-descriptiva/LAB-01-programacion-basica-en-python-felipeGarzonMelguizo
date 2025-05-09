764a107cc068bdf74a59a523946e3a7155d89f87
felipeGarzonMelguizo <69770704+Flying-Spaghetti-Monster69@users.noreply.github.com>
2025-05-09 12:09:34 -0500

    respuesta pregunta 01

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,6 +5,7 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+import csv
 
 def pregunta_01():
     """
@@ -14,3 +15,13 @@ def pregunta_01():
     214
 
     """
+
+    column_values = []
+    
+    with open('files/input/data.csv', 'r') as file:
+        csv_reader = csv.reader(file, delimiter='\t') 
+        
+        for row in csv_reader:
+            column_values.append(int(row[1]))  
+            
+    return sum(column_values)
